e47e05378c824dd27a0634db95325b5f4978f002
Magnus Løwer <magnus.lower@gmail.com>
2024-10-18 17:07:16 +0200

    Changed the project structure so that it matches the suggested structure which is alternative 2 (on the task description)

---
+++ global/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "4832bdd9-c568-497f-af42-3c45d1e16ef6"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
